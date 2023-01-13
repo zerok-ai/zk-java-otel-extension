78e850e75fe6d17ea9d45302a2bc4af21a241e51
rajeevzerok <113543563+rajeevzerok@users.noreply.github.com>
2023-01-13 14:19:43 +0530

    first commit

---
+++ extension/src/main/java/ai/zerokelvin/javaagent/mysqlinstrumentation/MySQLCommentInstrumentationModule.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package ai.zerokelvin.javaagent.mysqlinstrumentation;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers;
+import java.util.Arrays;
+import java.util.List;
+import net.bytebuddy.matcher.ElementMatcher;
+
+/**
+ * This is a demo instrumentation which hooks into servlet invocation and modifies the http
+ * response.
+ */
+@AutoService(InstrumentationModule.class)
+public final class MySQLCommentInstrumentationModule extends InstrumentationModule {
+  public MySQLCommentInstrumentationModule() {
+    super("jdbc");
+  }
+
+  @Override
+  public int order() {
+    return 1;
+  }
+
+  @Override
+  public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+    return AgentElementMatchers.hasClassesNamed("java.sql.Statement");
+  }
+
+  @Override
+  public List<TypeInstrumentation> typeInstrumentations() {
+    return Arrays.asList(
+        new MySQLPreparedStatementCommentInstrumentation(),
+        new MySQLStatementCommentInstrumentation());
+  }
+
+  //  public List<TypeInstrumentation> typeInstrumentations() {
+  //    return singletonList(new MySQLAddCommentInstrumentation());
+  //  }
+}
