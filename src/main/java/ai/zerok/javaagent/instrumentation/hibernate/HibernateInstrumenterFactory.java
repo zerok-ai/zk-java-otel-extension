3d6f832b0d538eac208d72a49d54458fca2de2ad
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2023-02-01 17:06:08 +0530

    add hibernate files

---
+++ src/main/java/ai/zerok/javaagent/instrumentation/hibernate/HibernateInstrumenterFactory.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package ai.zerok.javaagent.instrumentation.hibernate;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
+import io.opentelemetry.javaagent.bootstrap.internal.InstrumentationConfig;
+
+public final class HibernateInstrumenterFactory {
+  static final boolean CAPTURE_EXPERIMENTAL_SPAN_ATTRIBUTES =
+      InstrumentationConfig.get()
+          .getBoolean("otel.instrumentation.hibernate.experimental-span-attributes", false);
+
+  public static Instrumenter<HibernateOperation, Void> createInstrumenter(
+      String instrumentationName) {
+    InstrumenterBuilder<HibernateOperation, Void> instrumenterBuilder =
+        Instrumenter.builder(
+            GlobalOpenTelemetry.get(), instrumentationName, HibernateOperation::getName);
+
+    if (CAPTURE_EXPERIMENTAL_SPAN_ATTRIBUTES) {
+      instrumenterBuilder.addAttributesExtractor(new HibernateExperimentalAttributesExtractor());
+    }
+
+    return instrumenterBuilder.buildInstrumenter();
+  }
+
+  private HibernateInstrumenterFactory() {}
+}
