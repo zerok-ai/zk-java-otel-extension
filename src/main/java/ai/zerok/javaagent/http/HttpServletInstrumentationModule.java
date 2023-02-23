2725f3657cdbb8eb25f18da1643c33fafc121bc1
Avin Goyal <avingoyal01@gmail.com>
2023-02-23 16:50:45 +0530

    http servlet extension to add trace headers in response when it is not present in request headers

---
+++ src/main/java/ai/zerok/javaagent/http/HttpServletInstrumentationModule.java
@@ -0,0 +1,32 @@
+package ai.zerok.javaagent.http;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+
+import java.util.Arrays;
+import java.util.List;
+
+@AutoService(InstrumentationModule.class)
+public class HttpServletInstrumentationModule extends InstrumentationModule {
+    public HttpServletInstrumentationModule() {
+        super("servlet-container");
+    }
+
+    @Override
+    public int order() {
+        return 1;
+    }
+
+    @Override
+    public List<TypeInstrumentation> typeInstrumentations() {
+        return Arrays.asList(
+                new HttpServletInstrumentation());
+    }
+
+    @Override
+    public boolean isHelperClass(String className) {
+        return className.startsWith("ai.zerok.javaagent.http") || className.equals("ai.zerok.javaagent.utils.Utils");
+    }
+
+}
