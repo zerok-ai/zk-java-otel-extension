ee0658277f6e4837d4ea186ad7cad9ded392aaf1
Shivam Nagar <shivam.nagar@outlook.com>
2023-05-26 11:55:22 +0530

    added timer to sync data to redis.

--- src/main/java/ai/zerok/javaagent/exporter/internal/RedisHandler.java
+++ src/main/java/ai/zerok/javaagent/exporter/internal/RedisHandler.java
@@ -9,6 +9,8 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Pipeline;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -18,13 +20,15 @@ public class RedisHandler {
     int redisDB = 3;
     int ttlInSeconds = 15 * 60; // Set the TTL to 15 mins
     int batchSize = 100; // Set the desired batch size
-    long durationMillis = 5000; // Set the desired duration in milliseconds
+    long durationMillis = 5 * 1000; // Set the desired duration for sync (In ms)
+    long timerSyncDuration = 30 * 1000; // Sync items in pipeline after this duration is elapsed.n (In ms)
 
     Gson gson = new GsonBuilder().create();
     Jedis jedis;
     Pipeline pipeline;
     int count; // Counter for tracking the number of items added
     long startTime; // Start time
+    Timer timer = new Timer();
 
     public RedisHandler() {
         jedis = new Jedis(redisHostName, redisPort);
@@ -32,6 +36,27 @@ public class RedisHandler {
         count = 0;
         startTime = System.currentTimeMillis();
         jedis.select(redisDB);
+        timer.schedule(syncTask, 0, timerSyncDuration);
+    }
+    TimerTask syncTask = new TimerTask() {
+        @Override
+        public void run() {
+            // This happens only if the data volume does not exceed the batchsize or a long duration has elapsed.
+            System.out.println("Pipeline synchronized on timer");
+            syncPipeline();
+        }
+    };
+
+    private void syncPipeline() {
+        if (count >= batchSize || System.currentTimeMillis() - startTime >= durationMillis) {
+            // Execute the pipeline
+            pipeline.sync();
+            System.out.println("Pipeline synchronized on batchsize/duration");
+
+            // Reset the counter and update the start time
+            count = 0;
+            startTime = System.currentTimeMillis();
+        }
     }
 
     public void putTraceData(String traceId, TraceDetails traceDetails) {
@@ -48,14 +73,7 @@ public class RedisHandler {
         pipeline.expire(traceId, ttlInSeconds);
 
         // Sync to redis server if batch size has exceeded or sync duration has elapsed.
-        if (count >= batchSize || System.currentTimeMillis() - startTime >= durationMillis) {
-            // Execute the pipeline
-            pipeline.sync();
-
-            // Reset the counter and update the start time
-            count = 0;
-            startTime = System.currentTimeMillis();
-        }
+        syncPipeline();
     }
 
     public void forceSync() {
