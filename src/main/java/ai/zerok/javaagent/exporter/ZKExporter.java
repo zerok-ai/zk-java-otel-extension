d70f5b0028b7132228b95810e5f855805d921f95
Shivam Nagar <shivam.nagar@outlook.com>
2023-08-22 15:50:30 +0530

    added source & destination IPs in OTel span

--- src/main/java/ai/zerok/javaagent/exporter/ZKExporter.java
+++ src/main/java/ai/zerok/javaagent/exporter/ZKExporter.java
@@ -4,11 +4,16 @@ import ai.zerok.javaagent.exporter.internal.RedisHandler;
 import ai.zerok.javaagent.exporter.internal.SpanDetails;
 import ai.zerok.javaagent.exporter.internal.TraceDetails;
 import com.google.gson.Gson;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import io.opentelemetry.sdk.trace.data.SpanData;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,6 +50,29 @@ public class ZKExporter implements SpanExporter {
         return result;
     }
 
+    private Pair getSourceDestIpPair(SpanData spanData, Attributes attributes) {
+        //extract local and remote IP from spanData
+        SpanKind spanKind = spanData.getKind();
+        String destIP = "";
+        String sourceIP = "";
+        if(spanKind == SpanKind.CLIENT) {
+            // set host IP address as source IP
+            InetAddress IP= null;
+            try {
+                IP = InetAddress.getLocalHost();
+            } catch (UnknownHostException e) {
+                System.out.println("Failed to get local IP address");
+                e.printStackTrace();
+            }
+            destIP = attributes.get(NET_SOCK_PEER_ADDR).toString();
+            sourceIP = IP.toString();
+        } else if(spanKind == SpanKind.SERVER) {
+            destIP = attributes.get(NET_SOCK_HOST_ADDR).toString();
+            sourceIP = attributes.get(NET_SOCK_PEER_ADDR).toString();
+        }
+        return Pair.of(sourceIP, destIP);
+    }
+
     private Map<String, TraceDetails> generateStoreForSpanData(Collection<SpanData> spanDataList) {
         for (SpanData spanData : spanDataList) {
             String traceId = spanData.getTraceId();
@@ -57,12 +85,21 @@ public class ZKExporter implements SpanExporter {
             spanDetails.setSpanKind(spanData.getKind());
             spanDetails.setStart_ns(spanData.getStartEpochNanos());
             spanDetails.setEnd_ns(spanData.getEndEpochNanos());
-            Attributes attributes = spanData.getAttributes();
 
             if(SET_SPAN_ATTRIBUTES) {
+                Attributes attributes = spanData.getAttributes();
+
+                Pair sourceDestIpPair = getSourceDestIpPair(spanData, attributes);
+                String sourceIP = sourceDestIpPair.getLeft().toString();
+                String destIP = sourceDestIpPair.getRight().toString();
+
+                Map<AttributeKey<?>, Object> attributesMap = attributes.asMap();
+                attributesMap.put(AttributeKey.stringKey("source.ip"), sourceIP);
+                attributesMap.put(AttributeKey.stringKey("dest.ip"), destIP);
+
                 Gson gson = new Gson();
                 try {
-                    spanDetails.setAttributes(gson.toJsonTree(attributes.asMap()).getAsJsonObject());
+                    spanDetails.setAttributes(gson.toJsonTree(attributesMap).getAsJsonObject());
                 } catch (Exception e) {
                     System.out.println("Failed to set span attributes. traceId: " + traceId + ", spanId: " + spanData.getSpanId());
                     e.printStackTrace();
--- src/main/java/ai/zerok/javaagent/http/dropwizard/HttpModifier.java
+++ src/main/java/ai/zerok/javaagent/http/dropwizard/HttpModifier.java
@@ -20,18 +20,6 @@ public class HttpModifier {
     }
 
     public static HttpServletResponse addTraceHeaders(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        //TraceState
-//        String currentTraceState = httpServletResponse.getHeader(Utils.getTraceStateKey());
-//        ZkTraceState zkTraceState = TraceUtils.extractZkTraceState(currentTraceState);
-//        String finalTraceState = "";
-//        if(zkTraceState != null){
-//            finalTraceState = TraceUtils.appendToTraceState(zkTraceState.getOthers(), Utils.getTraceState());
-//        }else{
-//            finalTraceState = Utils.getTraceState();
-//        }
-//
-//        httpServletResponse.setHeader(Utils.getTraceStateKey(), finalTraceState);
-
         //TraceParent
         boolean isTraceStatePresent = isHeaderPresent(Utils.getTraceParentKey(), httpServletRequest);
         if(!isTraceStatePresent){
@@ -46,37 +34,5 @@ public class HttpModifier {
         }
         return httpServletResponse;
     }
-
-//    public static HttpServletResponse addTraceHeaders(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-//        Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
-//        boolean isTracestatePresent = false;
-//        while(headerNames.hasMoreElements()){
-//            String requestheaderName = headerNames.nextElement();
-//            if(requestheaderName.equals(Utils.getTraceStateKey())){
-//                isTracestatePresent = true;
-//            }
-//        }
-//
-//        isTracestatePresent = isTracestatePresent || httpServletResponse.getHeaderNames().contains(Utils.getTraceStateKey());
-////        if(!isTracestatePresent) {
-////            Collection<String> responseHeaderNames = httpServletResponse.getHeaderNames();
-////            isTracestatePresent = responseHeaderNames.contains(Utils.getTraceStateKey());
-////        }
-//
-//        if(!isTracestatePresent){
-//            Span span = Java8BytecodeBridge.currentSpan();
-//            SpanContext spanContext = span.getSpanContext();
-//            String spanId = spanContext.getSpanId();
-//            String traceId = spanContext.getTraceId();
-//            String parentSpandId = Utils.getParentSpandId(span);
-//            String traceState = Utils.getTraceState(spanId);
-//            String traceParent = Utils.getTraceParent(traceId, spanId);
-//            httpServletResponse.addHeader(Utils.getTraceParentKey(), traceParent);
-//            httpServletResponse.addHeader(Utils.getTraceStateKey(), traceState);
-//            System.out.println("Adding trace headers");
-//        }
-//
-//        return httpServletResponse;
-//    }
 
 }
