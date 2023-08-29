b6381061d289be241d1877f9ad95172c98868804
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-08-29 11:00:33 +0530

    Commenting out java 11 code in HttpClient

--- src/main/java/ai/zerok/javaagent/http/HttpClientInstrumentation.java
+++ src/main/java/ai/zerok/javaagent/http/HttpClientInstrumentation.java
@@ -2,14 +2,8 @@ package ai.zerok.javaagent.http;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.extendsClass;
 import static net.bytebuddy.matcher.ElementMatchers.*;
-
-import ai.zerok.javaagent.utils.Utils;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.javaagent.bootstrap.Java8BytecodeBridge;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
-import java.net.http.HttpRequest;
-import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
@@ -22,27 +16,27 @@ public class HttpClientInstrumentation implements TypeInstrumentation {
 
     @Override
     public void transform(TypeTransformer transformer) {
-        System.out.println("Inside ps- 1.2-http client....");
-
-        transformer.applyAdviceToMethod(
-                isMethod().and(isPublic()).and(namedOneOf("send", "sendAsync")),
-                this.getClass().getName() + "$SendAdvice");
-
-        System.out.println("Inside ps- 1.3-http client....");
+//        System.out.println("Inside ps- 1.2-http client....");
+//
+//        transformer.applyAdviceToMethod(
+//                isMethod().and(isPublic()).and(namedOneOf("send", "sendAsync")),
+//                this.getClass().getName() + "$SendAdvice");
+//
+//        System.out.println("Inside ps- 1.3-http client....");
     }
 
-    public static class SendAdvice {
-
-        @Advice.OnMethodEnter(suppress = Throwable.class)
-        public static void methodEnter(@Advice.Argument(value = 0, readOnly = false) HttpRequest httpRequest) {
-            Span span = Java8BytecodeBridge.currentSpan();
-            String parentSpanId = Utils.getParentSpandId(span);
-            String traceState = Utils.getTraceState(parentSpanId);
-            if (traceState != null ){
-                //httpRequest.headers().;
-                //httpRequest = HttpRequest.newBuilder(httpRequest, (n,v) -> true).header(Utils.getTraceStateKey(),traceState).build();
-            }
-        }
-    }
+//    public static class SendAdvice {
+//
+//        @Advice.OnMethodEnter(suppress = Throwable.class)
+//        public static void methodEnter(@Advice.Argument(value = 0, readOnly = false) HttpRequest httpRequest) {
+//            Span span = Java8BytecodeBridge.currentSpan();
+//            String parentSpanId = Utils.getParentSpandId(span);
+//            String traceState = Utils.getTraceState(parentSpanId);
+//            if (traceState != null ){
+//                //httpRequest.headers().;
+//                //httpRequest = HttpRequest.newBuilder(httpRequest, (n,v) -> true).header(Utils.getTraceStateKey(),traceState).build();
+//            }
+//        }
+//    }
 }
 
